7371c6ff3f35c22801db33e7b4c51730f9afac3e
EniolaCodes <adebimpeniola@gmail.com>
2026-01-07 11:19:21 +0100

    fix

--- prodev-mobile-app-0x00/app/index.tsx
+++ prodev-mobile-app-0x00/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>This is the main screen.</Text>
+      <Text>This is the main screen</Text>
     </View>
   );
 }
